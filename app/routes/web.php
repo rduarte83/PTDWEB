0646548dca8419bd0f7c6d527dc118e03d60b7b9
João Novo <joao.novo@nuisol.com>
2018-11-05 21:28:51 +0000

    Routes | Template
    - Added new routes
    - Added new Template

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,16 +1,26 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+// Home
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+// User
+Route::get('/register', "UserController@registerView");
+Route::get('/login', "UserController@loginView");
+Route::get('/admin', "UserController@adminView");
+
+// Products
+Route::get('/products/', "ProductController@showAll");
+Route::get('/products/{id}', "ProductController@show");
+Route::get('/products/add/{id}', "ProductController@addToCart");
+
+// User Autentication
+Route::post('/register', "UserController@create");
+Route::post('/login', "UserController@login");
+
+// Telemoveis
+/*Route::get("/telemovel/show", "TelemovelController@showAll");
+Route::get("/telemovel/show/{id}", "TelemovelController@show");
+Route::get("/telemovel/create", "TelemovelController@create");
+Route::post("/telemovel/store", "TelemovelController@store");*/
