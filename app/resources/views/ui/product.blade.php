791f0d9093b3fdd64cfa66a75f2bddc87e26d89b
João Novo <joao.novo@nuisol.com>
2018-12-11 15:31:44 +0000

    product route

--- app/resources/views/ui/product.blade.php
+++ app/resources/views/ui/product.blade.php
@@ -22,7 +22,7 @@
             </div>
 
             <div class="block2-txt p-t-20">
-                <a href="/product" class="block2-name dis-block s-text3 p-b-5">
+                <a href="{{URL("product")}}" class="block2-name dis-block s-text3 p-b-5">
                     Botija de gás Propano - GALP
                 </a>
                 <span class="block2-price m-text6 p-r-5">
